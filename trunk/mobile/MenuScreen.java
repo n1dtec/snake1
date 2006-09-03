92d9530ba1c48ee4b266b29197dc3a7b2bd2a311
gabriel.barros <gabriel.barros@71d2ac08-811c-0410-b8f5-5191b43d0857>
2006-09-03 03:23:22 +0000

    ready to receive the game

--- trunk/mobile/MenuScreen.java
+++ trunk/mobile/MenuScreen.java
@@ -1,6 +1,6 @@
 package mobile;
 
-import javax.microedition.lcdui.Canvas;
+/*import javax.microedition.lcdui.Canvas;
 import javax.microedition.lcdui.Graphics;
 
 public class MenuScreen extends Canvas {
@@ -17,4 +17,16 @@ public class MenuScreen extends Canvas {
 
 	}
 
+}
+*/
+
+import javax.microedition.lcdui.*;
+
+public class MenuScreen extends Canvas {
+   public void paint(Graphics g) {
+      g.setColor(255, 0, 0);
+      g.fillRect(0, 0, getWidth(), getHeight(  ));
+      g.setColor(255, 255, 255);
+      g.drawString("Hello World!", 0, 0, g.TOP | g.LEFT);
+   }
 }
