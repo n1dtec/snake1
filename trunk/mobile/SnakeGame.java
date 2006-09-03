d13e18179220a82104fae2251635564d4377abe4
gabriel.barros <gabriel.barros@71d2ac08-811c-0410-b8f5-5191b43d0857>
2006-09-03 03:27:42 +0000

    cleaning a little...

---
+++ trunk/mobile/SnakeGame.java
@@ -0,0 +1,30 @@
+package mobile;
+
+import javax.microedition.lcdui.Canvas;
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Graphics;
+
+public class SnakeGame extends Canvas implements CommandListener {
+
+	public SnakeGame() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void paint(Graphics arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void commandAction(Command arg0, Displayable arg1) {
+		// TODO Auto-generated method stub
+
+	}
+	public void resume(){
+	}
+	public void pause(){
+	}
+
+}
